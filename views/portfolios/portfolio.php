7f3300b62a2fe52c76ce0843d8b258eda1702f24
faisalahammed044274 <setu2014@gmail.com>
2022-07-30 22:18:03 +0600

    PHP MVC

--- views/portfolios/portfolio.php
+++ views/portfolios/portfolio.php
@@ -8,21 +8,36 @@
     <title>Portfolio</title>
 </head>
 <style>
-h1 {
-    background: green;
-    color: white;
-    padding: 10px;
-    text-align: center;
-}
+    h1 {
+        background: green;
+        color: white;
+        padding: 10px;
+        text-align: center;
+    }
 
-h2 {
-    background: gray;
-    color: white;
-    padding: 60px;
-}
+    h2 {
+        background: gray;
+        color: white;
+        padding: 60px;
+    }
+
+    .list {
+        background: yellow;
+        color: green;
+        padding: 18px;
+        border-radius: 10px;
+    }
 </style>
 
 <body>
+    <div class="list">
+        <?php
+        echo "<h3>Portfolio list :</h3>";
+        foreach ($portfolios as $portfolio) :  ?>
+            <li><?php echo $portfolio['title']; ?></li>
+        <?php endforeach; ?>
+    </div>
+
     <h1>Portfolio</h1>
     <h2>Hello from view with controller.</h2>
 </body>
